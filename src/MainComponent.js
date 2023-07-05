7b5b84e7bc36abd9ca692c0e755334af33405892
Kru1l <paarl0814@gmail.com>
2023-07-06 02:54:55 +0300

    init master

---
+++ src/MainComponent.js
@@ -0,0 +1,11 @@
+import styles from './MainComponent.module.css'
+
+const MainComponent = () => {
+
+    return (
+        <div className={styles.component}>
+        </div>
+    )
+}
+
+export default MainComponent;
