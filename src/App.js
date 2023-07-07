7cfaad810a91db8244e5fe22c3755b01f81cf1e6
Kru1l <paarl0814@gmail.com>
2023-07-08 01:34:44 +0300

    hw_1.2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
 import Character from "./component_character/Character";
+import {characters} from "./component_character/Character";
 
 function App() {
     return (
         <div className="App">
 
-            <Character id={6} name={'Abadango Cluster Princess'} status={'Alive'} species={'Alien'} gender={'Female'}
-                       image={'https://rickandmortyapi.com/api/character/avatar/6.jpeg'}/>
-
-            <Character id={500} name={'Fascist Mr. President'} status={'Alive'} species={'Human'} gender={'Male'}
-                       image={'https://rickandmortyapi.com/api/character/avatar/500.jpeg'}/>
-
-            <Character id={323} name={'Slaveowner'} status={'Dead'} species={'Human'} gender={'Male'}
-                       image={'https://rickandmortyapi.com/api/character/avatar/323.jpeg'}/>
-
-            <Character id={777} name={'Gotron Pilot'} status={'Dead'} species={'Human'} gender={'Male'}
-                       image={'https://rickandmortyapi.com/api/character/avatar/777.jpeg'}/>
-
-            <Character id={568} name={'Slut Dragon'} status={'Alive'} species={'Mythological Creature'} gender={'Male'}
-                       image={'https://rickandmortyapi.com/api/character/avatar/568.jpeg'}/>
+            {characters.map(person =>
+                <Character key={person.id} person={person}/>)}
 
         </div>
     );
--- src/component_character/Character.js
+++ src/component_character/Character.js
@@ -1,20 +1,42 @@
 import styles from './Character.module.css'
 
-const Character = ({id, name, status, species, gender, image}) => {
-
+export const characters = [
+    {
+        id: 6, name: 'Abadango Cluster Princess', status: 'Alive', species: 'Alien', gender: 'Female',
+        image: 'https://rickandmortyapi.com/api/character/avatar/6.jpeg'
+    },
+    {
+        id: 500, name: 'Fascist Mr. President', status: 'Alive', species: 'Human', gender: 'Male',
+        image: 'https://rickandmortyapi.com/api/character/avatar/500.jpeg'
+    },
+    {
+        id: 323, name: 'Slaveowner', status: 'Dead', species: 'Human', gender: 'Male',
+        image: 'https://rickandmortyapi.com/api/character/avatar/323.jpeg'
+    },
+    {
+        id: 777, name: 'Gotron Pilot', status: 'Dead', species: 'Human', gender: 'Male',
+        image: 'https://rickandmortyapi.com/api/character/avatar/777.jpeg'
+    },
+    {
+        id: 568, name: 'Slut Dragon', status: 'Alive', species: 'Mythological Creature', gender: 'Male',
+        image: 'https://rickandmortyapi.com/api/character/avatar/568.jpeg'
+    }
+];
+const Character = (props) => {
+    const {person} = props;
     return (
         <div className={styles.person}>
-            <h2>{name}</h2>
+
+            <h2>{person.name}</h2>
 
             <div className={styles.text}>
-                <p>id: {id}</p>
-                <p>Status: {status}</p>
-                <p>Species: {species}</p>
-                <p>Gender: {gender}</p>
+                <p>id: {person.id}</p>
+                <p>Status: {person.status}</p>
+                <p>Species: {person.species}</p>
+                <p>Gender: {person.gender}</p>
             </div>
 
-            <img src={image} alt={name}/>
-
+            <img src={person.image} alt={person.name}/>
 
         </div>
     )
